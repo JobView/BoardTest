8b1da6364bb8d4386c6d3ddbba7de0b7b082cf1f
wangzhenfei <179576267@qq.com>
2017-06-28 22:57:36 +0800

    家里修改的

---
+++ app/src/main/java/com/wzf/boardgame/function/http/dto/request/RegisterRequestDto.java
@@ -0,0 +1,62 @@
+package com.wzf.boardgame.function.http.dto.request;
+
+/**
+ * Created by wzf on 2017/6/28.
+ */
+
+public class RegisterRequestDto {
+
+    /**
+     * nickname : 昵称
+     * smsCode : 5871
+     * userId : 1000145
+     * userPwd : 123456
+     * userMobile : 13814591112
+     */
+
+    private String nickname;
+    private String smsCode;
+    private String userId;
+    private String userPwd;
+    private String userMobile;
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getSmsCode() {
+        return smsCode;
+    }
+
+    public void setSmsCode(String smsCode) {
+        this.smsCode = smsCode;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserPwd() {
+        return userPwd;
+    }
+
+    public void setUserPwd(String userPwd) {
+        this.userPwd = userPwd;
+    }
+
+    public String getUserMobile() {
+        return userMobile;
+    }
+
+    public void setUserMobile(String userMobile) {
+        this.userMobile = userMobile;
+    }
+}
